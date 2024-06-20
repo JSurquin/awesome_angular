f049c876f03197d5df50aa6cc95a5896789597ad
Tonytux <tony.aumont@gmail.com>
2024-06-20 15:21:38 +0200

    commentaires dans le fichier main

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
+// Voici comment commenter, 
+/// slashhhhh
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { AppComponent } from './app/app.component'; //cela permet d'importer le composant principal de l'app
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
